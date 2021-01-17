fa256bf7b97229c889c0ca44d01a42ab45cfe479
Ferret <54555281+FerretBoiX@users.noreply.github.com>
2021-01-17 13:28:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bot = new Discord.Client();
 var rings = "720247688301379664"
 
 var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function() {
+bot.listen(port, "0.0.0.0", function() {
 	console.log("Listening on Port 3000");
 });
 

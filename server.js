23ee5e854db9fd2997c2395582437731ec5263d1
Ferret <54555281+FerretBoiX@users.noreply.github.com>
2021-05-14 23:27:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
-const http = require("http");
-const express = require("express");
-const app = express();
 const Discord = require("discord.js");
 const TOKEN = process.env.TOKEN;
 
-var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function() {
-	console.log("Listening on Port 3000");
-});
-
-var random;
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
